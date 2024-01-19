c1105e46f6dc0b28b2956b6b904ecbbc2687c39a
Farah <farah.elyamany1234@gmail.com>
2024-01-19 16:56:38 +0200

    Added 3 files

---
+++ sheets/sheet1/C.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    long long a , b;
+
+    cin >> a >> b ;
+    cout << a << " + " << b << " = " << a + b << "\n";
+    cout << a << " * " << b << " = " << a * b << "\n";
+    cout << a << " - " << b << " = " << a - b;
+
+}
